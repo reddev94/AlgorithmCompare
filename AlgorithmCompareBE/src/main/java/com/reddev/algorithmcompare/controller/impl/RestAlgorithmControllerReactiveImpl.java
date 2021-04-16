5230d73bc884effa360d3bc8258487dd54b88dbc
reddev94 <davide.vennari@gmail.com>
2021-04-16 16:11:06 +0200

    deleted unused import

--- AlgorithmCompareBE/src/main/java/com/reddev/algorithmcompare/controller/impl/RestAlgorithmControllerReactiveImpl.java
+++ AlgorithmCompareBE/src/main/java/com/reddev/algorithmcompare/controller/impl/RestAlgorithmControllerReactiveImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/reactive")
