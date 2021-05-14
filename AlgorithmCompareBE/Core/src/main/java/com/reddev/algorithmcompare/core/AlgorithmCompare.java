e88e494a3f93c9510d91621a58510f33cc164995
reddev94 <davide.vennari@gmail.com>
2021-05-14 14:30:37 +0200

    cache fix

--- AlgorithmCompareBE/Core/src/main/java/com/reddev/algorithmcompare/core/AlgorithmCompare.java
+++ AlgorithmCompareBE/Core/src/main/java/com/reddev/algorithmcompare/core/AlgorithmCompare.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 import java.util.List;
 
@@ -18,7 +17,6 @@ import java.util.List;
         "com.reddev.algorithmcompare.core",
         "com.reddev.algorithmcompare.commons",
         "com.reddev.algorithmcompare.dao"})
-@EnableCaching
 public class AlgorithmCompare implements CommandLineRunner {
     protected Logger logger = LoggerFactory.getLogger(AlgorithmCompare.class);
 
--- AlgorithmCompareFE/src/app/app-routing.module.ts
+++ AlgorithmCompareFE/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlgorithmcompareComponent } from './algorithmcompare/algorithmcompare.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'AlgorithmCompareHome', component: AlgorithmcompareComponent },
+                        { path: '', redirectTo: '/AlgorithmCompareHome', pathMatch: 'full' }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
